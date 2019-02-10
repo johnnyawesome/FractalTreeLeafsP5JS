770849a8ff34f70b93e9c0b05fbad2657dc17265
Johnny Awesome <35241451+johnnyawesome@users.noreply.github.com>
2019-02-10 14:47:29 +0100

    Add files via upload

---
+++ FractalTreeLeafsP5JS/FractalTreeLeafsP5JS/sketch.js
@@ -0,0 +1,77 @@
+/// <reference path="../TSDef/p5.global-mode.d.ts" />
+
+let lineLength = 150;
+let x = 250;
+let y = 500;
+let branchAngle = null;
+let strokeGreenColor = 255;
+let branchThickness = 10;
+let bailout = 10;
+
+function setup() {
+  createCanvas(1200, 900, P2D);
+  stroke(0, strokeGreenColor, 0);
+  strokeWeight(branchThickness);
+}
+
+function draw() {
+  translate(width / 2, height);
+  background(0);
+  branchAngle = map(mouseX, 0, height, 1, 100);
+  drawTree(lineLength);
+}
+
+function drawTree(lineLength) {
+
+  //Coloring leafes
+  if (lineLength < 15) {
+    strokeGreenColor = map(lineLength, 0, 150, 1, 255);
+    stroke(0, 150, 0, 100);
+    strokeWeight(9);
+  }
+  //Coloring the branch
+  if (lineLength > 15) {
+    strokeGreenColor = map(lineLength, 0, 150, 1, 255);
+    stroke(139, 69, 19, strokeGreenColor);
+    branchThickness = map(lineLength, 0, 150, 0, 25);
+    strokeWeight(branchThickness);
+  }
+
+
+
+  if (lineLength >= bailout) {
+
+    line(0, 0, 0, -lineLength * 2);
+
+    translate(0, -lineLength * 2);
+
+    //Right branch 1
+    push();
+    rotate(radians(branchAngle));
+    line(0, 0, 0, -lineLength);
+    drawTree(lineLength * 0.65);
+    pop();
+
+    //Left branch 1
+    push();
+    rotate(radians(-branchAngle));
+    line(0, 0, 0, -lineLength);
+    drawTree(lineLength * 0.65);
+    pop();
+
+    //Right branch 2
+    push();
+    rotate(radians(branchAngle * 0.5));
+    line(0, 0, 0, -lineLength);
+    drawTree(lineLength * 0.5);
+    pop();
+
+    //Left branch 2
+    push();
+    rotate(radians(-branchAngle * 0.5));
+    line(0, 0, 0, -lineLength);
+    drawTree(lineLength * 0.7);
+    pop();
+
+  }
+}
